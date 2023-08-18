b87879062868a6027d630b5e065b005f1ebaf765
neuromaxer <neuromaxed@gmail.com>
2023-08-18 22:01:32 +0200

    update models and views

--- finance_manager/trades/models.py
+++ finance_manager/trades/models.py
@@ -3,6 +3,8 @@ from django.db import models
 class Trade(models.Model):
     symbol = models.CharField(max_length=10)
     quantity = models.PositiveIntegerField()
-    purchase_price = models.FloatField()
-    current_price = models.FloatField(null=True)
+    purchase_price = models.DecimalField(max_digits=18, decimal_places=3)
+    current_price = models.DecimalField(null=True, decimal_places=3, max_digits=18)
+    type = models.CharField(max_length=4, null=True)
+    time = models.CharField(max_length=20, null=True)
     
--- finance_manager/trades/views.py
+++ finance_manager/trades/views.py
@@ -5,6 +5,8 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
 import yfinance as yf 
+from collections import defaultdict
+from decimal import Decimal
 
 # Create your views here.
 
@@ -13,8 +15,28 @@ def trade_list(request):
     if request.method == "GET":
         trades = Trade.objects.all()
         trade_list = [model_to_dict(trade) for trade in trades]
-        print(trade_list)
-        return JsonResponse(trade_list, safe=False)
+        print('Trade List: ', trade_list)
+        open_positions = defaultdict(lambda: 0)
+
+        for trade in trade_list:
+            print("Trade: ", trade)
+            if trade["type"] == "buy":
+                open_positions[trade['symbol']] += trade['quantity']
+            elif trade["type"] == "sell":
+                open_positions[trade['symbol']] -= trade['quantity']
+            else:
+                raise Exception(f"Invalid trade type {trade['type']}")
+
+        open_positions_list = [{'symbol': symbol, 'quantity': quantity} for symbol, quantity in open_positions.items()] if open_positions else []
+
+        response_data = {
+            'trades': trade_list,
+            'open_positions': open_positions_list,
+        }
+
+        print("Response data: ", response_data)
+
+        return JsonResponse(response_data, safe=False)
     
     elif request.method == "POST":
         data = json.loads(request.body)
@@ -22,26 +44,31 @@ def trade_list(request):
 
         # get current price from yfinance
         ticker = yf.Ticker(data['symbol'])
-        current_price = ticker.history().tail(1)['Close'].iloc[0]
+        current_price = round(ticker.history().tail(1)['Close'].iloc[0], 3)
 
         print("Current price: ", current_price)
-        print("Purchase price: ", float(data['price']))
+        print("Purchase price: ", Decimal(data['price']))
 
         # create a new trade instance
-        new_trade = Trade(symbol=data['symbol'], quantity=int(data['quantity']), purchase_price=float(data['price']), current_price=current_price)
+        new_trade = Trade(
+            symbol=data['symbol'], 
+            quantity=int(data['quantity']),
+            purchase_price=Decimal(str(data['price'])),
+            current_price=Decimal(str(current_price)), 
+            type=data['type'],
+            time=data['time']
+            )
 
         # validate and save the new trade instance
-        try:
-            new_trade.full_clean()
-            new_trade.save()
-            trades = Trade.objects.all()
-            trade_list = [model_to_dict(trade) for trade in trades]
-            print(trade_list)
-            return JsonResponse(model_to_dict(new_trade), status=201)
-        
-        except Exception as e:
-            return JsonResponse({'error': str(e)}, status=400)
-        
+        print("New trade: ", new_trade)
+        new_trade.full_clean()
+        new_trade.save()
+        trades = Trade.objects.all()
+        trade_list = [model_to_dict(trade) for trade in trades]
+        print(trade_list)
+        return JsonResponse(model_to_dict(new_trade), status=201)
+    
+    
 
     else:
         raise Exception("Unsupported method")
