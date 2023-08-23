cf7f8b44cd1fe72dbe7eb6cd881a7328b22b6e45
neuromaxer <neuromaxed@gmail.com>
2023-08-23 21:49:55 +0200

    abstract logic from views in trade_table

--- backend/src/trade_table.py
+++ backend/src/trade_table.py
@@ -2,13 +2,19 @@ from trades.models import Trade
 from django.forms.models import model_to_dict
 from collections import defaultdict
 from typing import List
+import yfinance as yf
+from decimal import Decimal
 
 
 class TradeTable:
     def __init__(self) -> None:
-        self.trade_list = [model_to_dict(trade) for trade in Trade.objects.all()]
+        self.trade_list = self.get_trade_list()
         self.open_positions_list = self.get_open_positions()
         print("Trade List: ", self.trade_list)
+
+    @staticmethod
+    def get_trade_list() -> List[dict]:
+        return [model_to_dict(trade) for trade in Trade.objects.all()]
 
     def get_open_positions(self) -> List[dict]:
         open_positions = defaultdict(lambda: 0)
@@ -32,3 +38,31 @@ class TradeTable:
         )
 
         return open_positions_list
+
+    def add_trade(self, trade_data: dict) -> None:
+        # create a new trade instance
+        current_price = self.get_current_price(trade_data)
+        new_trade = Trade(
+            symbol=trade_data["symbol"],
+            quantity=int(trade_data["quantity"]),
+            purchase_price=Decimal(str(trade_data["price"])),
+            current_price=Decimal(str(current_price)),
+            type=trade_data["type"],
+            time=trade_data["time"],
+        )
+
+        # validate and save the new trade instance
+        print("New trade: ", new_trade)
+        new_trade.full_clean()
+        new_trade.save()
+        self.trade_list = self.get_trade_list()
+        return new_trade
+
+    @staticmethod
+    def get_current_price(trade_data: dict):
+        ticker = yf.Ticker(trade_data["symbol"])
+        current_price = round(ticker.history().tail(1)["Close"].iloc[0], 3)
+
+        print("Current price: ", current_price)
+        print("Purchase price: ", Decimal(trade_data["price"]))
+        return current_price
--- backend/trades/views.py
+++ backend/trades/views.py
@@ -1,10 +1,7 @@
-from .models import Trade
 from django.forms.models import model_to_dict
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
-import yfinance as yf
-from decimal import Decimal
 from src.trade_table import TradeTable
 
 
@@ -23,33 +20,10 @@ def trade_list(request):
         return JsonResponse(response_data, safe=False)
 
     elif request.method == "POST":
-        data = json.loads(request.body)
-        print("Posted data: ", data)
+        trade_data = json.loads(request.body)
+        print("Posted trade data: ", trade_data)
 
-        # get current price from yfinance
-        ticker = yf.Ticker(data["symbol"])
-        current_price = round(ticker.history().tail(1)["Close"].iloc[0], 3)
-
-        print("Current price: ", current_price)
-        print("Purchase price: ", Decimal(data["price"]))
-
-        # create a new trade instance
-        new_trade = Trade(
-            symbol=data["symbol"],
-            quantity=int(data["quantity"]),
-            purchase_price=Decimal(str(data["price"])),
-            current_price=Decimal(str(current_price)),
-            type=data["type"],
-            time=data["time"],
-        )
-
-        # validate and save the new trade instance
-        print("New trade: ", new_trade)
-        new_trade.full_clean()
-        new_trade.save()
-        trades = Trade.objects.all()
-        trade_list = [model_to_dict(trade) for trade in trades]
-        print(trade_list)
+        new_trade = trade_table.add_trade(trade_data)
         return JsonResponse(model_to_dict(new_trade), status=201)
 
     else:
